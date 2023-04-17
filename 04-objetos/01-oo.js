73edd9c53f4506d9c30ed4204f04581aba2dad78
Matheus Antonio <matheusantonio232@gmail.com>
2023-04-17 09:13:02 -0300

    iniciando objetos

---
+++ 04-objetos/01-oo.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+// Código não executável
+
+// Procedural
+processamento(valor1, valor2, valor3);
+
+// Orientação Objetos (OO)
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento() {
+  },
+};
+
+objeto.processamento();
+
+/**
+ *  Paradigmas de POO:
+ *  Abstração
+ *  Encapsulamento
+ *  Herança
+ *  Polimorfismo
+ */
