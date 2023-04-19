7087fab0e3701b3d3e9fcb4ef28865a2bfaa7388
Matheus Antonio <matheusantonio232@gmail.com>
2023-04-18 22:25:25 -0300

    primeiro modulo - node

---
+++ 07-node/01-moduleCliente.js
@@ -0,0 +1,12 @@
+const firstModule = require("./01-moduleA");
+const secondModule = require("./01-moduleB");
+
+console.log(module);
+console.log(firstModule);
+console.log(firstModule.boaNoite());
+console.log(firstModule.bomDia);
+
+console.log(secondModule);
+console.log(secondModule.ola);
+console.log(secondModule.bemVindo);
+console.log(secondModule.ateLogo);
